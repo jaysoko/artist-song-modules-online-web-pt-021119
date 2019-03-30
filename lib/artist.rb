8e487fb02269c007d3a5583c2b5bb23229cfa0bf
Jay Sokolowski <jaysoko@gmail.com>
2019-03-30 16:09:51 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -3,6 +3,8 @@ require 'pry'
 class Artist
   extend Memorable::ClassMethods
   extend Findable::ClassMethods
+  include Paramable
+  include Memorable::InstanceMethods
   attr_accessor :name
   attr_reader :songs
 
